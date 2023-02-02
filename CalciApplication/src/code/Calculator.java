c6c8a8e59f1741091bbef72a4fa6a7539901e2a7
unknown <shubhamashtaputre12@gmail.com>
2023-02-02 15:43:21 +0530

    adding calculator application source code

---
+++ CalciApplication/src/code/Calculator.java
@@ -0,0 +1,25 @@
+package code;
+
+public class Calculator {
+	
+	/*
+	 * Author: Shubham Ashtaputre
+	 * 
+	 * here, for example I'm creating only 3 methods i.e.
+	 * addition, subtraction, division
+	 *
+	 */
+	
+	public static int addition(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public static int subtraction(int num1, int num2) {
+		return num1-num2;
+	}
+	
+	public static int division(int num1, int num2) {
+		return num1/num2;
+	}
+	
+}
